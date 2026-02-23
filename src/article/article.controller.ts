37ad7a691770b7c6d5e5df8c1e1580a2ebddf148
Agustin Tabarcache <agustintabarcache74@gmail.com>
2026-02-23 16:23:19 -0300

    Agregue el Response, y la generazion del slug de Article

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Get, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { User } from 'src/user/decorator/user.decorator';
 import { UserEntity } from 'src/user/user.entity';
 import { CreateArticleDto } from './dto/createArticle.dto';
-import { ArticleEntity } from './article.entity';
 import { AuthGuard } from 'src/user/guards/user.guard';
+import { IArticleResponse } from './types/articleResponse.interface';
 
 @Controller('articles')
 export class ArticleController {
@@ -16,7 +24,11 @@ export class ArticleController {
   async createArticle(
     @User() user: UserEntity,
     @Body('article') createArticleDto: CreateArticleDto,
-  ): Promise<ArticleEntity> {
-    return await this.articleService.createArticle(user, createArticleDto);
+  ): Promise<IArticleResponse> {
+    const newArticle = await this.articleService.createArticle(
+      user,
+      createArticleDto,
+    );
+    return this.articleService.generateArticleResponse(newArticle);
   }
 }
